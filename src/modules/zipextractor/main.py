86222cf0a410ba4435d28da15c3722b0d0d1fe22
Borja Pintos Castro <bpintos@gradiant.org>
2021-02-06 19:17:23 +0100

    whatthefile#13 añadido diccionario de password y extración en directorio safe

--- src/modules/zipextractor/main.py
+++ src/modules/zipextractor/main.py
@@ -7,7 +7,8 @@ from src.domain.targetfile import TargetFile
 from src.domain.targetpath import TargetPath
 from src.modules.imodule import IModule
 
-PARAM_PWD = 'pwd'
+PARAM_PWD_DICT = 'pwd_dict'
+EXTRACTED_OUTPUT_PATH = 'extracted_output_path'
 
 
 class Constructor(IModule):
@@ -15,39 +16,67 @@ class Constructor(IModule):
     def __init__(self):
         super().__init__()
         self._name = "zipextractor"
-        self._help = """Extract files from zip:
-		Params: {'""" + PARAM_PWD + """': <password>] password to extract files"""
+        self._help = """Extract files from zip"""
         self._author = "BorjaPintos"
 
-    def _run(self, target_file : TargetPath, pwd):
+    def _run(self, target_file : TargetFile, extracted_output_path:str, pwd_list: list):
         result = {}
         binary = None
+        decripted_password = None
         with ZipFile(target_file.get_path(), 'r') as zip:
             for info in zip.infolist():
                 try:
-                    with zip.open(info.filename, 'r', pwd=pwd) as file:
+                    with zip.open(info.filename, 'r') as file:
                         binary = file.read()
                 except:
-                    try:
-                        with AESZipFile(target_file.get_path(), 'r') as zf:
-                            zf.pwd = pwd
-                            binary = zf.read(info.filename)
-                    except:
-                        result['error'] = "Bad password or wrong decrypted algorithm"
-                        continue
-
-                safe_name = info.filename.replace("..", ".")
-                path_to_save = os.path.join(target_file.get_directory(), safe_name)
-
-                with open(path_to_save, "wb+") as file:
-                    file.write(binary)
-                result[safe_name] = os.path.join(target_file.get_directory(), safe_name)
+                    pass
+                if not binary:
+                    "probamos con contraseña"
+                    for pwd in pwd_list:
+                        try:
+                            binary = self.decrypt_with_password(info.filename, pwd)
+                            if binary:
+                                decripted_password = pwd
+                                break;
+                        except:
+                            pass
+                if not binary:
+                    "probamos con contraseña y algoritmo de cifrado AES"
+                    for pwd in pwd_list:
+                        try:
+                            binary = self.decrypt_AES_WithPassword(target_file, info.filename, pwd)
+                            if binary:
+                                decripted_password = pwd
+                                break;
+                        except:
+                            pass
+                if binary:
+                    safe_name = info.filename.replace("..", ".")
+                    path_to_save = os.path.join(extracted_output_path, safe_name)
+                    result[info.filename] = path_to_save
+                    if decripted_password:
+                        result["password"] = decripted_password.decode()
+                    with open(path_to_save, "wb+") as file:
+                        file.write(binary)
+                else:
+                    result["error"] = "Bad password or wrong decrypted algorithm"
 
         return result
 
-    def _get_pwd_param(self, params):
-        if PARAM_PWD in params:
-            return params[PARAM_PWD]
+
+    def decrypt_with_password(self, file_name, pwd):
+        with zip.open(file_name, 'r', pwd=pwd) as file:
+            binary = file.read()
+        return binary
+
+    def decrypt_AES_WithPassword(self, target_file: TargetFile, file_name, pwd):
+        try:
+            with AESZipFile(target_file.get_path(), 'r') as zf:
+                zf.pwd = pwd
+                binary = zf.read(file_name)
+                return binary
+        except:
+            return None
 
     def is_valid_for(self, target_file: TargetPath):
         if target_file.is_file():
@@ -55,7 +84,27 @@ class Constructor(IModule):
                 return True
         return False
 
+    def _get_password_list(self, params):
+        list = []
+        if PARAM_PWD_DICT in params:
+            password_list_path = params[PARAM_PWD_DICT]
+            with open(password_list_path, "r") as password_list_file:
+                line = password_list_file.readline().strip()
+                while line:
+                    list.append(line.encode())
+                    line = password_list_file.readline().strip()
+
+        return list
+
+    def _get_extracted_output_path(self, params):
+        if not EXTRACTED_OUTPUT_PATH in params:
+            return None
+        return params[EXTRACTED_OUTPUT_PATH]
+
     def run(self, target_file: TargetFile):
-        pwd = self._get_pwd_param(self.get_params())
-        report = self._run(target_file, pwd.encode())
+        extracted_output_path = self._get_extracted_output_path(self.get_params())
+        if not extracted_output_path:
+            return {"error" : "property: " + EXTRACTED_OUTPUT_PATH + " is required for unzip"}
+        pwd_list = self._get_password_list(self.get_params())
+        report = self._run(target_file, extracted_output_path, pwd_list)
         return report
--- tests/modules/zipextractortest.py
+++ tests/modules/zipextractortest.py
@@ -9,13 +9,13 @@ import os
 class ZipExtractorTest(unittest.TestCase):
 
     def test_unzip(self):
-        final_file = "./tests/examples/Surprise.txt"
+        params = {"extracted_output_path" : "./tests/examples/safe_directory"}
+        final_file = os.path.join(params["extracted_output_path"], "Surprise.txt")
+
         if os.path.exists(final_file):
             os.remove(final_file)
         self.assertFalse(os.path.exists(final_file))
-
         path = "./tests/examples/Surprise.txt.zip"
-        params = {"pwd": "surprise"}
         target_file = TargetFile(path)
         module = Constructor()
         self.assertTrue(module.is_valid_for(target_file))
@@ -26,30 +26,34 @@ class ZipExtractorTest(unittest.TestCase):
         os.remove(final_file)
 
     def test_unzip_with_password(self):
-        final_file = "./tests/examples/Surprise2.txt"
+        params = {"extracted_output_path" : "./tests/examples/safe_directory",
+                  "pwd_dict" : "./tests/examples/wordlist.txt"}
+        final_file = os.path.join(params["extracted_output_path"], "Surprise2.txt")
         if os.path.exists(final_file):
             os.remove(final_file)
-        self.assertFalse(os.path.exists(final_file))
 
+        self.assertFalse(os.path.exists(final_file))
         path = "./tests/examples/Surprise2.txt.zip"
-        params = {"pwd": "surprise"}
         target_file = TargetFile(path)
         module = Constructor()
         module.set_params(params)
         self.assertTrue(module.is_valid_for(target_file))
         result = module.run(target_file)
+        print(result)
+        print(final_file)
         self.assertEqual(result["Surprise2.txt"], final_file)
+        self.assertEqual(result["password"], "surprise")
         self.assertTrue(os.path.exists(final_file))
         os.remove(final_file)
 
     def test_error_password(self):
-        final_file = "./tests/examples/Surprise2.txt"
+        params = {"extracted_output_path" : "./tests/examples/safe_directory",
+                  "pwd_dict" : "./tests/examples/Prueba.c"}
+        final_file = os.path.join(params["extracted_output_path"], "/Surprise2.txt")
         if os.path.exists(final_file):
             os.remove(final_file)
         self.assertFalse(os.path.exists(final_file))
-
         path = "./tests/examples/Surprise2.txt.zip"
-        params = {"pwd": "surpriseaaaa"}
         target_file = TargetFile(path)
         module = Constructor()
         module.set_params(params)
