683860d8104f72499f3d0502d600f7550f440ee0
Borja Pintos Castro <bpintos@gradiant.org>
2021-02-06 18:11:40 +0100

    whatthefile#14 añadido una nueva forma de output

--- src/output/listoutput.py
+++ src/output/listoutput.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+from src.domain.whatthefileconfiguration import WhatTheFileConfiguration
 from src.output.ioutput import IOutput
 
 
 class ListOutput(IOutput):
 
-    def __init__(self, conf: dict = None):
+    def __init__(self, conf: WhatTheFileConfiguration = None):
         self._list = []
 
     def dump(self, element: dict):
--- src/output/outputfactory.py
+++ src/output/outputfactory.py
@@ -1,16 +1,19 @@
 from src.domain.whatthefileconfiguration import WhatTheFileConfiguration
 from src.output.listoutput import ListOutput
 from src.output.printoutput import PrintOutput
+from src.output.fileoutput import FileOutput
 
 
 class OutputFactory:
 
     @staticmethod
     def get_output(conf: WhatTheFileConfiguration):
-            if "output" in conf.get_whatthefile_secction():
-                output = conf.get_whatthefile_secction()["output"]
-                if output == "print":
-                    return PrintOutput()
-                if output == "list":
-                    return ListOutput()
-            return PrintOutput()
+        if "output" in conf.get_whatthefile_secction():
+            output = conf.get_whatthefile_secction()["output"]
+            if output == "print":
+                return PrintOutput(conf)
+            if output == "list":
+                return ListOutput(conf)
+            if output == "file":
+                return FileOutput(conf)
+        return PrintOutput(conf)
