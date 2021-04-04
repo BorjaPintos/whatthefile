fec2150d11b3b93ac2bd991c39bd9cddba331d09
Borja Pintos Castro <bpintos@gradiant.org>
2021-04-04 12:27:35 +0200

    whatthefile#42 Prueba de instalación de tesseract en windows

--- .github/actions/modules/modules.sh
+++ .github/actions/modules/modules.sh
@@ -27,8 +27,9 @@ elif [ "$RUNNER_OS" == "macOS" ]; then
   brew install tesseract
 else
   choco install tesseract --pre
-  refreshenv
-  tesseract --version
+  tesseract.exe --version
+  tesseract-ocr-w32-setup-v5.0.0-alpha.20201127.exe --version
+  ls C:\Program Files\Tesseract-OCR
 fi
 
 #install module qrreader
