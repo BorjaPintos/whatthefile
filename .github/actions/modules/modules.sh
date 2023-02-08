660b0bcbc6f4a3b23de5051e9ec6a2f8edabbf4a
Borja Pintos <borjapintoscastro@gmail.com>
2023-02-08 09:32:08 +0100

    Update modules.sh
    Actualizado el workflow

--- .github/actions/modules/modules.sh
+++ .github/actions/modules/modules.sh
@@ -45,7 +45,7 @@ fi
 
 #install module whisper
 if [ "$RUNNER_OS" == "Linux" ]; then
-  sudo apt-get install ffmpeg
+  sudo apt-get install -y ffmpeg
 elif [ "$RUNNER_OS" == "macOS" ]; then
   brew install ffmpeg
 elif [ "$RUNNER_OS" == "Windows" ]; then
