ce310471e26fbbecbd9d3db3470c6aab809a82aa
Borja Pintos <borjapintoscastro@gmail.com>
2024-07-17 12:46:14 +0200

    Update modules.sh

--- .github/actions/modules/modules.sh
+++ .github/actions/modules/modules.sh
@@ -8,7 +8,7 @@ EXIFTOOL_VERSION=12.89
 if [ "$RUNNER_OS" == "Windows" ]; then
   curl --output exiftool.zip --url https://exiftool.org/exiftool-$EXIFTOOL_VERSION'_64'.zip
   unzip -qq ./exiftool.zip -d "./exiftool"
-  cp "./exiftool/exiftool(-k).exe" "./exiftool/exiftool.exe"
+  cp "./exiftool/exiftool-$EXIFTOOL_VERSION'_64'/exiftool(-k).exe" "./exiftool/exiftool.exe"
   echo "./exiftool/" >> $GITHUB_PATH
 else
   wget https://exiftool.org/Image-ExifTool-$EXIFTOOL_VERSION.tar.gz --no-check-certificate
